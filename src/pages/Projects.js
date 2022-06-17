c3348959adb1eb94298fc2f6192a8562df20a042
David Aydin <david.aydin6@gmail.com>
2022-06-17 19:40:32 -0400

    projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
+
 function Projects(props) {
-    return(
-        <div>
-            <h1>Projects Page</h1>
-        </div>
-    )
+    const [projects, setProjects] = useState(null);
+
+    const getProjectsData = async () => {
+        const response = await fetch(props.URL + "projects");
+        const data = await response.json();
+        setProjects(data);
+    };
+
+    useEffect(() => getProjectsData(), []);
+
+    const loaded = () => {
+        return projects.map((project) => (
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.img} alt="img"/>
+                <a href={project.git}>
+                    <button>live site</button>
+                </a>
+            </div>
+        ));
+    };
+
+    return projects ? loaded() : <h1>Loading...</h1>;
 }
 
 export default Projects;
